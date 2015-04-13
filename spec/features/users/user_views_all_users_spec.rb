f0c69d4b7b9cbf41d0452747b9aed3b08f52e3ea
Mafooch <jpmaff@yahoo.com>
2015-04-13 10:51:12 -0400

    Changed test to use sign_in_as helper method

--- spec/features/users/user_views_all_users_spec.rb
+++ spec/features/users/user_views_all_users_spec.rb
@@ -3,24 +3,22 @@ require 'rails_helper'
 feature 'user views a list of all users', %Q{
   As a signed in user I want to see a list of all users and be able to
   navigate to their profiles
-},focus: true do
+} do
   scenario "signed in user views a list of all users and can navigate to
     their profiles" do
     user = FactoryGirl.create(:user)
+    sign_in_as user
+
     user_one = FactoryGirl.create(:user)
     user_two = FactoryGirl.create(:user)
 
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
     visit users_path
 
     expect(page).to have_content(user_one.username)
     expect(page).to have_content(user_two.username)
 
     click_link(user_one.username)
-    expect(page).to have_content(user_one.about_me)    
+    expect(page).to have_content(user_one.about_me)
   end
 
   scenario 'guest unsuccessfully attempts to veiw all users' do
